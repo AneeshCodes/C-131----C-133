e840cffb1b8c720728fc68f734c53128ad013674
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-12-14 18:04:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function clock(){
+  window.location = "Clock/Clock.html"
+}
+
+function orange(){
+  window.location = "Orange/Orange.html"
+}
+
+function sofa(){
+  window.location = "Sofa/Sofa.html"
+}
+
+function tablet(){
+  window.location = "Tablet/Tablet.html"
+}
+
+function tv(){
+  window.location = "Person/TV.html"
+}
